23a641589d3813efa0cc40203ed2393fe6b7b85d
Antonin Messinger <antonin.messinger@people-doc.com>
2016-01-18 17:05:06 +0100

    fixup! Allowing for custom broccoli environments

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var envs = ['production', 'development']
 function getEnv () {
   var env = process.env.BROCCOLI_ENV || 'development'
 
-  if (! (envs.indexOf(env) > -1) ) {
+  if (envs.indexOf(env) === -1) {
     throw new Error('Environment set to "' + env + '", but BROCCOLI_ENV only accepts one of the following values: ' + envs.join(', '))
   }
 
