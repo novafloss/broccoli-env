4d0e0e5f8204e82481659120f275f6433243b52e
Antonin Messinger <antonin.messinger@people-doc.com>
2016-01-20 18:01:24 +0100

    Making a more robust logic

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
-exports.getEnv = getEnv
-exports.setEnvs = setEnvs
+var defaultEnvs = ['development', 'production']
 
-var envs = ['development', 'production']
+function Env() {
+  return this.init()
+}
 
-function getEnv () {
-  var env = process.env.BROCCOLI_ENV
-  if (!env) {
-    if (envs.length > 0) {
-      env = envs[0];
+Env.prototype = {
+  init: function() {
+    this.setEnvs(defaultEnvs)
+
+    if (process.env.BROCCOLI_ENV) {
+      this.setEnv(process.env.BROCCOLI_ENV)
     }
+
+    return this
+  },
+
+  getEnv: function () {
+    return this.env
+  },
+
+  setEnv: function(env) {
+    if (this.getEnvs().indexOf(env) === -1) {
+      throw new Error('Environment set to "' + env + '", but BROCCOLI_ENV only accepts one of the following values: ' + this.getEnvs().join(', '))
+    }
+
+    this.env = env
+
+    return this
+  },
+
+  getEnvs: function() {
+    return this.envs
+  },
+
+  setEnvs: function(envs) {
+    this.envs = envs
+
+    if (!this.getEnvs().length) {
+      throw new Error('Parameter "envs" should be an array')
+    }
+
+    this.setEnv(this.envs[0])
+
+    return this
   }
+};
 
-  if (envs.indexOf(env) === -1) {
-    throw new Error('Environment set to "' + env + '", but BROCCOLI_ENV only accepts one of the following values: ' + envs.join(', '))
-  }
-
-  return env
-}
-
-function setEnvs(e) {
-  envs = e;
-  return this;
-}
+module.exports = (function(options) {
+  return new Env(options)
+})()
